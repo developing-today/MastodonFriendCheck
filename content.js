ee0df9f3e1199bb390cdd9568cc913a721e81446
Drewry Pope <drewrypope@gmail.com>
2017-06-01 14:02:31 -0500

    0.0.7 new followlist method from CSV

--- content.js
+++ content.js
@@ -1,22 +1,17 @@
 // Copyright (c) 2017 Drewry Pope. All rights reserved.
 
 var currentUrl = window.location.href;
-var followingList = [];
-chrome.storage.sync.get('FollowingList', function (result)
-{
-	alert(followingList.length)
-    if (result.FollowingList != "null") followingList = result.FollowingList;
-	alert(followingList.length)
 
-    if (followingList)
-    {
-		followingList.forEach(function(element)
-		{
-			if (currentUrl == element)
-			{
-        alert(element)
+chrome.storage.sync.get('FollowingList', function (result) {
+  if (result.FollowingList != "null") {
+	  result.FollowingList.forEach(function(element) {
+      parts = element.split("@");
+      name = parts[parts.length - 2];
+      domain = parts[parts.length - 1];
+
+			if (currentUrl.includes(domain) && (currentUrl.includes("@" + name) || currentUrl.includes("users/" + name + "/"))) {
 				document.getElementsByClassName('button')[0].childNodes[0].nodeValue = "Following";
 			}
-		});
+  	});
 	}
 });
--- popup.js
+++ popup.js
@@ -1,110 +1,51 @@
 // Copyright (c) 2017 Drewry Pope. All rights reserved.
 
-//TODO:
-//Pull from follows.csv and parse that into array to send to chromelocalstore
-//https://social.tchncs.de/settings/exports/follows.csv
-
-document.addEventListener("DOMContentLoaded", function()
-{
+document.addEventListener("DOMContentLoaded", function() {
 	var submitButtonElement = document.getElementById("submitButton");
 
-	chrome.storage.sync.get('FullyDesignatedAddress', function(result)
-	{
-    	if (result.FullyDesignatedAddress)
-    	{
-    		name = result.FullyDesignatedAddress;
-    		document.getElementById("userIdLabel").innerHTML = result.FullyDesignatedAddress ;
-    	}
+	chrome.storage.sync.get('FullyDesignatedAddress', function(result) {
+  	if (result.FullyDesignatedAddress) {
+  		name = result.FullyDesignatedAddress;
+  		document.getElementById("userIdLabel").innerHTML = result.FullyDesignatedAddress;
+  	}
 
-		if (document.getElementById("userIdLabel").innerHTML != "@drewry@social.tchncs.de")
-		{
+		if (document.getElementById("userIdLabel").innerHTML != "@drewry@social.tchncs.de") {
 			document.getElementById("userIdTextBox").value = document.getElementById("userIdLabel").innerHTML;
 		}
 	});
 
-	if (submitButtonElement)
-	{
-		submitButtonElement.addEventListener("click", function()
-		{
-			var input;
-			var parts;
-			var name;
-			var domain;
-			var url;
+	if (submitButtonElement) {
+		submitButtonElement.addEventListener("click", function() {
+			var input = document.getElementById("userIdTextBox").value;
+			var parts = input.split("@");
+			var name = parts[parts.length - 2];
+			var domain = "https://" + parts[parts.length - 1];
+			var url = domain + "/settings/exports/follows.csv";
+			var xmlHttp = new XMLHttpRequest();
 			var followingList = [];
-
-			input = document.getElementById("userIdTextBox").value;
-			parts = input.split("@");
-			name = parts[parts.length - 2];
-			domain = "https://" + parts[parts.length - 1];
-			url = domain + "/users/" + name + "/following";
-			recursiveAddAllFollowing(url);
-
-			function recursiveAddAllFollowing(inputUrl)
-			{
-				var xmlHttp = new XMLHttpRequest();
-
-				xmlHttp.open("GET", inputUrl);
-
-				xmlHttp.onreadystatechange = function()
-				{
-					var xml;
-					var handle = [];
-					var re = /@[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+/;
-					var reLocal = /@[a-zA-Z0-0._-]+/;
-
-					if(xmlHttp.readyState === XMLHttpRequest.DONE && xmlHttp.status === 200)
-					{
-						if (xmlHttp.responseText == "")
-						{
-							throw new Error("Empty response.");
-							alert("Empty XML");
-						}
-						xml = (new DOMParser()).parseFromString(xmlHttp.responseText, "text/html");
-						for (var i = 1; i < xml.getElementsByClassName("name").length; i++)
-						{
-							handle.push(String(re.exec(xml.getElementsByClassName("name")[i].childNodes[1].text)));
-
-							if (handle[handle.length - 1] == "null")
-							{
-								handle[handle.length - 1] = String(reLocal.exec(xml.getElementsByClassName("name")[i].childNodes[1].text));
-							}
-						}
-						for (var iHandle = 0; iHandle < handle.length; iHandle++)
-						{
-							var inputParts = handle[iHandle].split("@");
-
-							if (inputParts.length == 3)
-							{
-								followingList.push("https://" + inputParts[inputParts.length - 1] + "/@" + inputParts[inputParts.length - 2]);
-							} else
-							{
-								followingList.push(domain  + "/@"  + inputParts[inputParts.length - 1]);
-							}
-						}
-						if (xml.getElementsByClassName("next").length != 0)
-						{
-							var localPath = xml.getElementsByClassName("next")[0].getElementsByTagName("a")[0].getAttribute("href")
-							if (localPath != null && localPath != undefined) recursiveAddAllFollowing(domain + localPath);
-						}
-						else
-						{			
-							setChromeStorage('FollowingList', followingList);
-							setChromeStorage('FullyDesignatedAddress', input);
-							document.getElementById("userIdLabel").innerHTML= input;
-							document.getElementById("userIdTextBox").value="Thanks!";
-							alert("Congratulations on having " + followingList.length + " people you follow!");	
-						}
+			
+			xmlHttp.onreadystatechange = function() {
+				if(xmlHttp.readyState === XMLHttpRequest.DONE && xmlHttp.status === 200) {
+					if (xmlHttp.responseText == "") {
+						throw new Error("Empty response.");
+						alert("Empty XML");
+					} else {
+						followingList = xmlHttp.responseText.split("\n")
+						setChromeStorage('FollowingList', followingList);
+						setChromeStorage('FullyDesignatedAddress', input);
+						document.getElementById("userIdLabel").innerHTML= input;
+						document.getElementById("userIdTextBox").value="Thanks!";
+						alert("Congratulations on having " + followingList.length + " people you follow!");	
 					}
-				};
-				xmlHttp.send();
-			}
+				}
+			};
+			xmlHttp.open("GET", url);
+			xmlHttp.send();
 		});
 	}
 });
 
-function setChromeStorage(name, value)
-{	
+function setChromeStorage(name, value) {
 	var dataObj = {};
 	dataObj[name] = value;
 	chrome.storage.sync.set(dataObj, function() {});
